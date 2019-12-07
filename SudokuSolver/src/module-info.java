4f16c1c19759d27e23f79dba5a75efe92eb6b0fe
RilindZejnullahu <rilind.zejnullahu@gmail.com>
2019-12-07 04:04:19 +0100

    Initial Commit

---
+++ SudokuSolver/src/module-info.java
@@ -0,0 +1,11 @@
+module SudokuSolver {
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.swing;
+	requires javafx.media;
+	requires javafx.graphics;
+	requires javafx.web;
+	requires javafx.swt;
+	exports sudoku;
+}
